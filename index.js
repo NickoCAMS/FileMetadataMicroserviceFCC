66007a918e568223f5b890e139cbbe8ebbbad4bc
Nicolo' Sartori <nsartori99@gmail.com>
2024-11-24 17:12:13 +0000

    First commit

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 var express = require('express');
 var cors = require('cors');
+const multer  = require('multer')
 require('dotenv').config()
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); 
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`); // Nome univoco per il file
+  },
+});
+
+const upload = multer({ storage });
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'File non fornito' });
+  }
+
+  const { originalname, mimetype, size } = req.file;
+
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size,
+  });
+});
 
 
 
